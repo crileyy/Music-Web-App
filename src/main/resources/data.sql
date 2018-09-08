867c12037be200ad44f712030c0f988ba4c9aab4
crileyy <criley0007@gmail.com>
2018-09-08 12:21:37 -0400

    Added more data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO SONGS (TITLE, ARTIST, YOUTUBE_ID, GENRE) VALUES ('Ill Try Anything Once', 'The Strokes', '0RDq5PKcN5g', 'rock');
+INSERT INTO SONGS (TITLE, ARTIST, YOUTUBE_ID, GENRE) VALUES ('Corduroy Dreams', 'Rex Orange County', 'oSl7I8ue400', 'indie');
+INSERT INTO SONGS (TITLE, ARTIST, YOUTUBE_ID, GENRE) VALUES ('Trying Your Luck', 'The Strokes', 'KadGvFjqGls', 'rock');
 INSERT INTO PLAYLISTS (TITLE) VALUES ('test');
